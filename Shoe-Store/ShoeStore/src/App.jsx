e7e4362e0fd4a8e45c2a54621b19b3baf7a72a50
Sai Parkav <86610494+SaiParkav@users.noreply.github.com>
2024-03-27 11:55:27 +0530

    changes in the hero jsx file and the product list page changes

--- Shoe-Store/ShoeStore/src/App.jsx
+++ Shoe-Store/ShoeStore/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => (
   
   <main className='relative'>
     <Nav/>
-    <section className='xl:passing-1 wide:padding-r padding-b'>
+    <section className='xl:passing-l wide:padding-r padding-b'>
       <Hero/> 
     
     </section>
