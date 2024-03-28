f293030972bf080f5d8ace556997757031e5294a
Sai Parkav <86610494+SaiParkav@users.noreply.github.com>
2024-03-28 11:35:54 +0530

    product list images and code

--- Shoe-Store/ShoeStore/src/constants/index.js
+++ Shoe-Store/ShoeStore/src/constants/index.js
@@ -1,5 +1,6 @@
 import { facebook, instagram, shieldTick, support, truckFast, twitter } from "../assets/icons";
 import { bigShoe1, bigShoe2, bigShoe3, customer1, customer2, shoe4, shoe5, shoe6, shoe7, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3 } from "../assets/images";
+import { AirForce1S1 , } from "../assets/productlist";
 
 export const navLinks = [
     { href: "#home", label: "Home" },
@@ -73,6 +74,16 @@ export const products = [
     },
 ];
 
+export const productlist = [
+    {
+        imgURL: AirForce1S1,
+        name: "Air Force 1 Mint Chocolate",
+        price: "Rs.11,000 /-"
+    },
+    {
+
+    }
+]
 export const services = [
     {
         imgURL: truckFast,
